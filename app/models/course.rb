672fe78d745d318cbf690adc433f596f9efc59e8
Brando Rondon <brando@brandeis.edu>
2016-03-21 14:25:08 -0400

    changed model for POSTGRES

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,9 +3,9 @@ class Course < ActiveRecord::Base
 
   def self.search(course , subject_id)
     if course != "" && subject_id != ""
-      where("name LIKE ? AND subject_id LIKE ?", "%#{course}%","%#{subject_id}%")
+      where("name LIKE ? AND subject_id LIKE ?", "%#{course}%",subject_id)
     elsif subject_id != "" && course == ""
-      where("subject_id LIKE ?", "%#{subject_id}%")
+      where("subject_id LIKE ?", subject_id)
     elsif course != "" && subject_id == ""
       where("name LIKE ?", "%#{course}%")
     else
